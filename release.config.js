19e9bf49d9fa82437649345921aaee0b081f7e90
George Drak <sonicgd@gmail.com>
2022-11-18 00:58:34 +0500

    feat(ci): add ci

---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  verifyConditions: ['@semantic-release/github'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github'
+  ],
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'master', 'main', 'next', 'next-major', {
+    name: 'beta',
+    prerelease: true
+  }, {name: 'alpha', prerelease: true}]
+};
